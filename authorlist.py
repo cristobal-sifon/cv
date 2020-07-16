b0612f89800d6136dbd48b0c8753cccdd9ae332a
Cristóbal Sifón <cristobal.sifon@pucv.cl>
2020-07-16 16:32:59 -0400

    printing improvements

--- authorlist.py
+++ authorlist.py
@@ -14,29 +14,47 @@ file = open(sys.argv[1])
 
 # words that should not be capitalized
 secondary = (
-    'a', 'about', 'and', 'around', 'between', 'by', 'for', 'from', 'in',
-    'int', 'of', 'on', 'over', 'the', 'through', 'to', 'via', 'with', 'z')
+    'a', 'about', 'an', 'and', 'around', 'at', 'between', 'by', 'for', 'from', 'in',
+    'int', 'near', 'of', 'on', 'other', 'out', 'over', 'the', 'through', 'to', 'via',
+    'with', 'without', 'z')
 # acronyms that should stay all upper case
 acronyms = (
     'ACT', 'ACTPol', 'ALMA', 'ATCA', 'BAHAMAS', 'CMB', 'EAGLE', 'GAMA', 'GMRT', 'HSC',
     'HST', 'KiDS', 'MCMC', 'PLCK', 'PSZ', 'PSZ2', 'SDSS', 'SPT', 'SZ', 'SZE')
+acronyms_lower = [i.lower() for i in acronyms]
+
+def format_word(word):
+    if word.lower() in secondary:
+        word = word.lower()
+    elif word.lower() in acronyms_lower:
+        word = acronyms[acronyms_lower.index(word.lower())]
+    else:
+        word = word.capitalize()
+    return word
 
 print()
 # first line(s) should be the title
 title = ''
-line = file.readline().replace('\n', '')
+line = file.readline().strip()
 while len(line) > 0:
   title += line + ' '
   line = file.readline().replace('\n', '')
-print(title.split()[0].capitalize(), end=' ')
-for word in title.split()[1:]:
-    if word.lower() in secondary:
-        print(word.lower(), end=' ')
-    elif word.upper() in acronyms:
-        print(word, end=' ')
+title = title.split()
+title[0] = title[0].capitalize()
+for i, word in enumerate(title[1:], 1):
+    for symbol in '-+/':
+        if symbol in word:
+            break
     else:
-        words = [w.capitalize() for w in word.split('-')]
-        print('-'.join(words), end=' ')
+        symbol = None
+    if symbol:
+        word = [format_word(w) for w in word.split(symbol)]
+        word = symbol.join(word)
+    else:
+        word = format_word(word)
+    title[i] = word
+title = ' ' .join(title)
+print(title)
 print('\n')
 
 # then comes the author list, copied directly from the paper
@@ -83,4 +101,8 @@ for line in file:
 
 print(alist)
 print('{0} authors'.format(nauth))
+print()
+print(r'{0} \etal{{{1}}}'.format(alist.split()[0][:-1], nauth))
+print(fr'\paper{{{title}}},')
+print(r'2020, \href{}{},')
 print()
